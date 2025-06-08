87bfd5f54efe7e9a5d7853827db5ab62edee0726
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-08 17:53:04 +0800

    refactor: enhance type hints and organize environment variable settings

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from pathlib import Path
 from enum import Enum
 
 from dotenv import load_dotenv
@@ -7,9 +8,11 @@ from loguru import logger
 
 from src.utils import check_env_variable
 
-PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
-ENV_FILE = PROJECT_ROOT / ".env"
-DATA_DIR = PROJECT_ROOT / "data"
+PROJECT_ROOT: Path = pathlib.Path(__file__).resolve().parent.parent
+ENV_FILE: Path = PROJECT_ROOT / ".env"
+DATA_DIR: Path = PROJECT_ROOT / "data"
+EXTERNAL_DATA_DIR: Path = DATA_DIR / "external"
+INTERIM_DATA_DIR: Path = DATA_DIR / "interim"
 
 if not ENV_FILE.exists():
     logger.warning(
@@ -19,11 +22,11 @@ else:
     load_dotenv(dotenv_path=ENV_FILE)
     logger.info(f"Loaded environment variables from {ENV_FILE}")
 
-required_env_vars = []
-non_essential_env_vars = []
+required_env_vars: list[str] = []
+non_essential_env_vars: list[str] = []
 
 for var in required_env_vars:
-    value = os.getenv(var)
+    value: str | None = os.getenv(var)
     check_env_variable(value, var, important=True)
 
 for var in non_essential_env_vars:
@@ -36,6 +39,9 @@ class Settings(Enum):
 
     # Add more environment variables as needed
     # EXAMPLE = os.getenv("example")
+    X_USERNAME = os.getenv("X_USERNAME", "")
+    X_PASSWORD = os.getenv("X_PASSWORD", "")
+    X_RAPIDAPI_KEY = os.getenv("X_RAPIDAPI_KEY", "")
 
 
 # Log key paths
