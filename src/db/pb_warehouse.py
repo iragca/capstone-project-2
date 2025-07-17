60d7d588d7620d7c1d2c8a115a5bf8a18c32c6ca
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-07-17 08:23:16 +0800

    refactor: enhance type annotations for methods in PBWarehouse class

--- src/db/pb_warehouse.py
+++ src/db/pb_warehouse.py
@@ -1,4 +1,5 @@
 from pocketbase import PocketBase
+from pocketbase.models import Record
 
 from src.config import Settings as s
 from src.models import Tweet, User
@@ -11,7 +12,7 @@ class PBWarehouse:
             email=s.POCKETBASE_EMAIL.value, password=s.POCKETBASE_PASSWORD.value
         )
 
-    def ingest_tweet(self, tweet: dict):
+    def ingest_tweet(self, tweet: dict) -> dict[str, Record]:
         assert isinstance(tweet, dict), "Input must be a dictionary"
         processed_tweet = self._process_tweet(tweet)
         processed_user = self._process_user(tweet)
@@ -23,7 +24,7 @@ class PBWarehouse:
         }
 
     @staticmethod
-    def _process_tweet(tweet: dict):
+    def _process_tweet(tweet: dict) -> dict[str, any]:
         assert isinstance(tweet, dict), "Input must be a dictionary"
 
         text = tweet.get("text", "")
@@ -49,25 +50,28 @@ class PBWarehouse:
         return parsed_tweet.model_dump()
 
     @staticmethod
-    def _process_user(tweet: dict):
+    def _process_user(tweet: dict) -> dict[str, any]:
         assert isinstance(tweet, dict), "Input must be a dictionary"
         user = tweet.get("user", {})
         parsed_user = User(**user)
         return parsed_user.model_dump()
 
-    def update_has_fetched_replies(self, tweet_id: str) -> None:
+    def update_has_fetched_replies(self, tweet_id: str) -> Record:
         assert isinstance(tweet_id, str), "tweet_id must be a string"
-        record = self.client.collection("tweets_v2").get_list(1, 1, {"filter": f"tweet_id = '{tweet_id}'"})
-        
+        record = self.client.collection("tweets_v2").get_list(
+            1, 1, {"filter": f"tweet_id = '{tweet_id}'"}
+        )
+
         updated_record = self.client.collection("tweets_v2").update(
-            record.items[0].id,
-            {"fetched_replies": True}
+            record.items[0].id, {"fetched_replies": True}
         )
         return updated_record
-    
-    def get_user_by_id(self, user_id: str):
+
+    def get_user_by_id(self, user_id: str) -> dict[str, any]:
         assert isinstance(user_id, str), "user_id must be a string"
-        user = self.client.collection("tweet_users").get_list(1, 1, {"filter": f"user_id = '{user_id}'"})
+        user = self.client.collection("tweet_users").get_list(
+            1, 1, {"filter": f"user_id = '{user_id}'"}
+        )
 
         if user.items and len(user.items) > 0:
             return user.items[0].__dict__
