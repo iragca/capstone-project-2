bcd853042806c8b787dc6b7b28343f3149ea6864
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-07-30 14:49:25 +0800

    feat: improve input validation for Twitter handle and user ID in Streamlit app

--- streamlit_mvp.py
+++ streamlit_mvp.py
@@ -91,12 +91,12 @@ with col1:
         x_handle = None
 
     if isinstance(x_handle, str):
-        if len(x_handle) == 0:
-            x_handle = None
-
         if x_handle.isdigit():
             st.error("Please enter a valid Twitter handle.")
             st.stop()
+        if len(x_handle) == 0:
+            x_handle = None
+
 
     strict_matching = st.checkbox(
         label="Use strict matching for username",
@@ -127,12 +127,12 @@ with col2:
         x_user_id = None
 
     if isinstance(x_user_id, str):
-        if len(x_user_id) == 0:
-            x_user_id = None
-
         if not x_user_id.isdigit():
             st.error("Please enter a valid user ID.")
             st.stop()
+        if len(x_user_id) == 0:
+            x_user_id = None
+
 
 if st.button(
     "Predict potential interactions with Extremist tweets",
