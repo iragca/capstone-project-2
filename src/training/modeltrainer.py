ed73402f8f6da1219b1cef5cde18dedc1a1c64e6
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-09-23 13:21:43 +0800

    feat: added more eval metrics

--- src/training/modeltrainer.py
+++ src/training/modeltrainer.py
@@ -2,8 +2,8 @@ import copy
 
 import mlflow
 import torch
+from sklearn.metrics import auc, f1_score, precision_score, recall_score, roc_auc_score
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score, f1_score
 
 
 class ModelTrainer:
@@ -19,7 +19,9 @@ class ModelTrainer:
         val_max = 0
         best_model = self.model
 
-        for epoch in tqdm(range(1, self.args["epochs"] + 1), desc="Training Epochs", ncols=100):
+        for epoch in tqdm(
+            range(1, self.args["epochs"] + 1), desc="Training Epochs", ncols=100
+        ):
             for i, batch in enumerate(self.dataloaders["train"]):
                 batch.to(self.device)
                 self.model.train()
@@ -38,25 +40,25 @@ class ModelTrainer:
                 loss.backward()
                 self.optimizer.step()
 
-                score_train = self.evaluate(self.dataloaders["train"])
-                score_val = self.evaluate(self.dataloaders["val"])
-                score_test = self.evaluate(self.dataloaders["test"])
 
-                mlflow.log_metrics(
-                    {
-                        "TRAIN: ROC-AUC": score_train["roc_auc"],
-                        "VAL: ROC-AUC": score_val["roc_auc"],
-                        "TEST: ROC-AUC": score_test["roc_auc"],
-                        "TRAIN: F1 Score": score_train["f1_score"],
-                        "VAL: F1 Score": score_val["f1_score"],
-                        "TEST: F1 Score": score_test["f1_score"],
-                        "Loss": loss.item(),
-                    },
-                    step=epoch,
-                )
+                metrics = {}
 
-                if val_max < score_val["roc_auc"]:
-                    val_max = score_val["roc_auc"]
+                for split in ["train", "val", "test"]:
+                    score = self.evaluate(self.dataloaders[split])
+                    metrics.update({
+                        f"{split.upper()}: ROC-AUC": score["roc_auc"],
+                        f"{split.upper()}: F1 Score": score["f1_score"],
+                        f"{split.upper()}: Precision": score["precision"],
+                        f"{split.upper()}: Recall": score["recall"],
+                        f"{split.upper()}: AUC": score["auc"]
+                    })
+
+                metrics["Loss"] = loss.item()
+
+                mlflow.log_metrics(metrics, step=epoch)
+
+                if val_max < metrics["VAL: ROC-AUC"]:
+                    val_max = metrics["VAL: ROC-AUC"]
                     best_model = copy.deepcopy(self.model)
 
         return best_model
@@ -65,6 +67,9 @@ class ModelTrainer:
         self.model.eval()
         score = 0
         f1_score_count = 0
+        recall_score_count = 0
+        precision_score_count = 0
+        auc_score_count = 0
         num_batches = 0
 
         for batch in dataloader:
@@ -79,14 +84,32 @@ class ModelTrainer:
             pred = torch.sum(nodes_first * nodes_second, dim=-1)
             pred = torch.sigmoid(pred)
 
-            score += roc_auc_score(
-                batch.edge_label.flatten().cpu().numpy(),
-                pred.flatten().data.cpu().numpy(),
-            )
+            y_true = batch.edge_label.flatten().cpu().numpy()
+            y_pred = pred.flatten().data.cpu().numpy()
             pred_labels = (pred > self.args["threshold"]).float()
+            y_labels_pred = pred_labels.flatten().data.cpu().numpy()
+
+            score += roc_auc_score(
+                y_true,
+                y_pred,
+            )
+            auc_score_count += auc(
+                y_true,
+                y_pred,
+            )
             f1_score_count += f1_score(
-                batch.edge_label.flatten().cpu().numpy(),
-                pred_labels.flatten().data.cpu().numpy(),
+                y_true,
+                y_labels_pred,
+                zero_division=0,
+            )
+            recall_score_count += recall_score(
+                y_true,
+                y_labels_pred,
+                zero_division=0,
+            )
+            precision_score_count += precision_score(
+                y_true,
+                y_labels_pred,
                 zero_division=0,
             )
             num_batches += 1
@@ -94,4 +117,7 @@ class ModelTrainer:
         return {
             "roc_auc": score / num_batches,
             "f1_score": f1_score_count / num_batches,
+            "recall": recall_score_count / num_batches,
+            "precision": precision_score_count / num_batches,
+            "auc": auc_score_count / num_batches,
         }
