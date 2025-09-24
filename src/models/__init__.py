c65171ade60cfc9527c42694fd85157b51d25604
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-09-24 15:12:13 +0800

    refactor: format __all__ declaration for better readability

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,14 @@
 from .customgraph import DiGraph, Graph
 from .features import Features
+from .inference import TweetLinkProbability
 from .tweet import Tweet
 from .user import User
-from .inference import TweetLinkProbability
 
-__all__ = ["Tweet", "User", "Features", "Graph", "DiGraph", "TweetLinkProbability"]
+__all__ = [
+    "Tweet",
+    "User",
+    "Features",
+    "Graph",
+    "DiGraph",
+    "TweetLinkProbability",
+]
